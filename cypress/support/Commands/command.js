08505c96cf563bd798f86a84ca25d52916d6bd77
Jai <obminajai@gmail.com>
2025-07-24 12:07:41 +0800

    updated screenshot command file

--- cypress/support/Commands/command.js
+++ cypress/support/Commands/command.js
@@ -1,18 +1,21 @@
-Cypress.Commands.add('captureScreenshot', () => {
+Cypress.Commands.add('captureScreenshot', (testTitle) => {
     const now = new Date();
-    //   const timestamp = now.toLocaleString('sv-SE', {
-    //     timeZone: 'Asia/Manila',
-    //     hour12: false, 
-    //   }).replace(/[^0-9]/g, '-');  
     const dateOnly = now.toLocaleDateString('sv-SE', {
         timeZone: 'Asia/Manila'
     }).replace(/[^0-9]/g, '-');
+
     const specPath = Cypress.spec.relative || 'unknown_spec';
     const specBaseName = specPath
         .split(/[\\/]/)
-        .pop()                             // Get the filename
-        .replace(/\.[^/.]+$/, '')          // Remove file extension
-        .replace(/[^a-zA-Z0-9]/g, '_');    // Sanitize to filename-safe
-    const filename = `${specBaseName}-${dateOnly}-Jai`;
+        .pop()
+        .replace(/\.[^/.]+$/, '')
+        .replace(/[^a-zA-Z0-9]/g, '_');
+
+    const sanitizedTitle = testTitle
+        ? testTitle.replace(/[^a-zA-Z0-9]/g, '_')
+        : 'unknownTest';
+
+    const filename = `${specBaseName}-${sanitizedTitle}-${dateOnly}-Jai`;
     cy.screenshot(filename, { capture: 'fullPage' });
 });
+
